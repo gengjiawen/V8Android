6fa7e873b5c2e1870678e9bbf7b349ee00411694
gengjiawen <technicalcute@gmail.com>
2018-09-22 17:03:08 +0800

    Initial commit

---
+++ app/src/main/assets/script/math.js
@@ -0,0 +1,11 @@
+//log("math module");
+//var a = require("forwardDir/b")
+
+function add (a,b){
+    return a + b;
+}
+
+
+exports.PI = 3.14;
+exports.add = add;
+// module.exports = add;
---
+++ app/src/main/java/com/boyaa/v8wrapper/MainActivity.java
@@ -0,0 +1,47 @@
+package com.boyaa.v8wrapper;
+
+import android.content.res.AssetManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        AssetManager mAssetManager = getAssets();
+        nativeInit(mAssetManager);
+
+        // Example of a call to a native method
+//        TextView tv = (TextView) findViewById(R.id.sample_text);
+//        tv.setText(stringFromJNI());
+
+//        executeJSScript();
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+
+    public native void executeJSScript();
+
+    public native void nativeDestroy();
+
+    public native void nativeInit(AssetManager assetManager);
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        nativeDestroy();
+    }
+}
